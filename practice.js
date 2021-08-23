6b6fd30f6760c7e812e7a98bb7adeb62e8345c42
mhselim103 <mhselin103@gmail.com>
2021-08-23 14:30:55 +0600

    string & array full file

---
+++ practice.js
@@ -0,0 +1,13 @@
+/* const task = "Practice Makes a person perfect";
+
+const index = task.toLowerCase().indexOf("M");
+console.log(index); */
+/* const str1 = 'Bangladesh is a beautiful country.';
+console.log(str1.endsWith(".")); */
+// console.log(str1.startsWith("Bangladesh"));
+/* const str = 'I love coding more than eating.';
+
+console.log(str.slice(2, 6)); */
+/* for (let i== 0; i < 10; i++) {
+    console.log(100);
+ } */
